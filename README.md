2d7d059b82c7bd658a735fbacfbf6d147b4aa590
Boris Mbarga <elhmn@users.noreply.github.com>
2019-03-03 17:24:17 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# codekeeper (ckp)
+Simple CLI that calls, keeps and fetches your useful scripts from any remote
---
+++ ckp/main.go
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*  main.go                                                                   */
+/*                                                                            */
+/*   By: elhmn <www.elhmn.com>                                                */
+/*             <nleme@live.fr>                                                */
+/*                                                                            */
+/*   Created: Sun Mar  3 17:59:45 2019                        by elhmn        */
+/*   Updated: Sun Mar 03 18:00:12 2019                        by bmbarga      */
+/*                                                                            */
+/* ************************************************************************** */
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("codekeeper (ckp)")
+}
