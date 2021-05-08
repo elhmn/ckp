31fb64b312e21997902cfc577aa3b10a5b0acb8a
elhmn <nleme@live.fr>
2021-05-08 13:13:42 +0200

    feat(store): add store code command

---
+++ cmd/store.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"github.com/elhmn/ckp/internal/config"
+	"github.com/spf13/cobra"
+)
+
+//NewStoreCommand stores everything that written after --code or --solution flag
+func NewStoreCommand(conf config.Config) *cobra.Command {
+	command := &cobra.Command{
+		Use:   "store",
+		Short: "`ckp store` will store your solution or code",
+		Long: `ckp store will store your solution or code in your solution repository
+
+	example: ckp store code 'echo je suis con'
+	Will store 'echo je suis con' as a code asset in your solution repository
+
+
+	example: ckp store solution 'https://opensource.code'
+	Will store 'https://opensource.code' as a solution asset in your solution repository
+`,
+	}
+
+	//Add commands
+	command.AddCommand(NewStoreCodeCommand(conf))
+	command.AddCommand(NewStoreSolutionCommand(conf))
+
+	//Set flags
+	command.PersistentFlags().StringP("path", "p", "", `ckp store -p <path_to_your_code_or_solution>`)
+	command.PersistentFlags().StringP("comment", "c", "", `ckp store -c <comment>`)
+	command.PersistentFlags().StringP("alias", "a", "", `ckp store -a <alias>`)
+
+	return command
+}
