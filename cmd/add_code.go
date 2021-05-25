c0f67da4bc7c6a69353bd474d39cb8df61af8645
elhmn <nleme@live.fr>
2021-05-25 21:49:10 +0200

    feat: add confirmation prompt for code addition

--- cmd/add_code.go
+++ cmd/add_code.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/briandowns/spinner"
 	"github.com/elhmn/ckp/internal/config"
+	"github.com/elhmn/ckp/internal/printers"
 	"github.com/elhmn/ckp/internal/store"
 	"github.com/spf13/cobra"
 	flag "github.com/spf13/pflag"
@@ -42,16 +43,25 @@ func NewAddCodeCommand(conf config.Config) *cobra.Command {
 }
 
 func addCodeCommand(cmd *cobra.Command, args []string, conf config.Config) error {
-	//Setup spinner
-	spin := spinner.New(spinner.CharSets[11], 100*time.Millisecond)
-	spin.Start()
-	defer spin.Stop()
-
 	if err := cmd.Flags().Parse(args); err != nil {
 		return err
 	}
 	flags := cmd.Flags()
 	code := strings.Join(args, " ")
+
+	//Check if the code entry contains sensitive data
+	if ret, word := store.HasSensitiveData(code); ret {
+		fmt.Fprintf(conf.OutWriter, "Contains the keyword `%s` in %s\n", word, code)
+		if !printers.Confirm("Add anyway ?") {
+			fmt.Fprintf(conf.OutWriter, "Code entry addition was aborted!\n")
+			return nil
+		}
+	}
+
+	//Setup spinner
+	spin := spinner.New(spinner.CharSets[11], 100*time.Millisecond)
+	spin.Start()
+	defer spin.Stop()
 
 	dir, err := config.GetStoreDirPath(conf)
 	if err != nil {
