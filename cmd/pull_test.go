61ce9d463859ecaea03cbc50729e29067303c811
elhmn <nleme@live.fr>
2021-05-28 17:19:42 +0200

    feat: add pull command

---
+++ cmd/pull_test.go
@@ -0,0 +1,41 @@
+package cmd_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/elhmn/ckp/cmd"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+)
+
+//TestPullCommand test the `ckp pull` command
+func TestPullCommand(t *testing.T) {
+	t.Run("pull successfully", func(t *testing.T) {
+		conf, mockedExec := createConfig(t)
+		writer := &bytes.Buffer{}
+		conf.OutWriter = writer
+
+		//Specify expectations
+		gomock.InOrder(
+			mockedExec.EXPECT().DoGit(gomock.Any(), "fetch", "origin", "main"),
+			mockedExec.EXPECT().DoGit(gomock.Any(), "diff", "origin/main", "--", gomock.Any()),
+			mockedExec.EXPECT().DoGit(gomock.Any(), "pull", "--rebase", "origin", "main"),
+		)
+
+		command := cmd.NewPullCommand(conf)
+		//Set writer
+		command.SetOutput(conf.OutWriter)
+
+		err := command.Execute()
+		if err != nil {
+			t.Errorf("Error: failed with %s", err)
+		}
+
+		exp := "\nckp store was pulled successfully\n"
+		got := writer.String()
+		if !assert.Equal(t, exp, got) {
+			t.Errorf("expected failure with [%s], got [%s]", exp, got)
+		}
+	})
+}
