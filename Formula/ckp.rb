7f24f4cc226ffa20673ac1f9aeb71e551d0caa4e
goreleaserbot <goreleaser@carlosbecker.com>
2021-08-31 13:14:07 +0200

    Brew formula update for ckp version v0.4.1

--- Formula/ckp.rb
+++ Formula/ckp.rb
@@ -5,28 +5,28 @@
 class Ckp < Formula
   desc ""
   homepage "https://github.com/elhmn/ckp"
-  version "0.4.0"
+  version "0.4.1"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/elhmn/ckp/releases/download/v0.4.0/ckp_0.4.0_darwin_amd64.tar.gz"
-      sha256 "b8083b1a567a779b1720c2e86d6722d5971d0e9245583e35bb1e3fd16328327c"
+      url "https://github.com/elhmn/ckp/releases/download/v0.4.1/ckp_0.4.1_darwin_amd64.tar.gz"
+      sha256 "2aecf8f994f931f9f3ed3e193afde1111adfcee8e51f86738b27c3ec7cb909a5"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/elhmn/ckp/releases/download/v0.4.0/ckp_0.4.0_darwin_arm64.tar.gz"
-      sha256 "0a3c1559c82bb40338bea6ede5b9fa410b5a269751fcd8b51a65eff153024631"
+      url "https://github.com/elhmn/ckp/releases/download/v0.4.1/ckp_0.4.1_darwin_arm64.tar.gz"
+      sha256 "873ebc9703ca279cadad66fd12321b220642293d5c91953f8b6233bb9cb2954c"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/elhmn/ckp/releases/download/v0.4.0/ckp_0.4.0_linux_amd64.tar.gz"
-      sha256 "aa47973cea82353182dc39663fd0c216a82bad9c7616263dd91c86340f93dffe"
+      url "https://github.com/elhmn/ckp/releases/download/v0.4.1/ckp_0.4.1_linux_amd64.tar.gz"
+      sha256 "8323278206ce3588ef584eb23503dee6928644e4e61ada0224093d231d3167d5"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/elhmn/ckp/releases/download/v0.4.0/ckp_0.4.0_linux_arm64.tar.gz"
-      sha256 "ebc4938a0a9003ec9fdd1c3ad7930ed8ef006ff71ef24cf6f14ae224196ad2db"
+      url "https://github.com/elhmn/ckp/releases/download/v0.4.1/ckp_0.4.1_linux_arm64.tar.gz"
+      sha256 "ef67fb1b1380d958f8ea807ceba772d72ddf8331823f135c3351051c0505ba33"
     end
   end
 
